22d2b4254cf954a446fb8c8b1420984b80c6ca68
Paden Pace <pjpace27@yahoo.com>
2017-04-30 19:38:14 -0500

    transition of blog posts from html to js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -26,9 +26,125 @@ var posts = [
 						"Creation of a basic D and D Character Creator sheet that stores to Firebase"]
 			}
 		]
+	},{
+		day: 3,
+		date: "April 3, 2017", 
+		projects: [
+			{
+				title: "Blog-Personal-Portfolio",
+				info: ["Creation of Blog Page to show my coding learning experience ",
+						"Layout of Blog page and the first Blog post regarding HTML and CSS"]
+			}
+		]
+	},{
+		day: 4,
+		date: "April 4, 2017", 
+		projects: [
+			{
+				title: "Blog-Personal-Portfolio",
+				info: ["Developing the styling for the Blog"]
+			},{
+				title: "Flashcard-Generator",
+				info: ["Basic layout of the individual pages of the generator"]
+			}
+		]
+	},{
+		day: 5,
+		date: "April 5, 2017", 
+		projects: [
+			{
+				title: "Flashcard-Generator",
+				info: ["Developing the Front/Back option .js and .txt files",
+						"Creating the basic files for Cloze option"]
+			}
+		]
+	},{
+		day: 6,
+		date: "April 6, 2017", 
+		projects: [
+			{
+				title: "Flashcard-Generator",
+				info: ["Developing the Cloze .js and .txt with simple functionality"]
+			},{
+				title: "Blog-Personal-Portfolio",
+				info: ["Addition of the 100 Day Coding Challenge panel to the blog"]
+			}
+		]
+	},{
+		day: 7,
+		date: "April 7, 2017", 
+		projects: [
+			{
+				title: "Flashcard-Generator",
+				info: ["Reformat Basic.tx to JSON object and addition of reader and writter",
+						"Seperation of the constructors to files"]
+			}
+		]
+	},{
+		day: 8,
+		date: "April 8, 2017", 
+		projects: [
+			{
+				title: "Flashcard-Generator",
+				info: ["Addition of options landing page with the npm inquirer"]
+			},{
+				title: "Bamazon, Node.js, MySQL",
+				info: ["Creation of node application used to access local Database"]
+			}
+		]
+	},{
+		day: 9,
+		date: "April 9, 2017", 
+		projects: [
+			{
+				title: "Bamazon, Node.js, MySQL",
+				info: ["Development of Bamazon Customer option",
+						"Addition of Constructor and MySQL database with table 'products'"]
+			}
+		]
+	},{
+		day: 10,
+		date: "April 10, 2017", 
+		projects: [
+			{
+				title: "Bamazon, Node.js, MySQL",
+				info: ["Creation and Development of Bamazon Manager option",]
+			}
+		]
+	},{
+		day: 11,
+		date: "April 11, 2017", 
+		projects: [
+			{
+				title: "Bamazon, Node.js, MySQL",
+				info: ["Creation and Development of Bamazon Supervisor option",
+						"Addition of Bamazon landing page 'Index'"]
+			}
+		]
+	},{
+		day: 12,
+		date: "April 12, 2017", 
+		projects: [
+			{
+				title: "Bamazon, Node.js, MySQL",
+				info: ["Addition of MySQL table 'departments'",
+						"Development and functionality to the Bamazon Sales Log"]
+			}
+		]
+	},{
+		day: 13,
+		date: "April 13, 2017", 
+		projects: [
+			{
+				title: "Blog-Personal-Portfolio",
+				info: ["Updates to 100 Day Coding Challenge and start of Blog Post #2"]
+			},{
+				title: "Personal-Portfolio",
+				info: ["Changes to portfolio style sheet to add functionailty and compatable styles across all pages"]
+			}
+		]
 	}
 ];
-
 
 
 
@@ -39,16 +155,53 @@ $(document).ready(function(){
 
 
 
+	for (i=0; i<posts.length; i++){
+		var challengePost = $("<div>");
+				challengePost.addClass("challenge-div");
+				challengePost.attr("id", "challenge-div"+i);
+				$("#inner-challenge-col").append(challengePost);
 
-	// var friendSection = $("<div>");
-	// 		friendSection.addClass("results");
-	// 		friendSection.addClass("panel");
-	// 		friendSection.attr("id", "bestFriend");
-	// 		$("#bestFriendDiv").html(friendSection);
-	// 		$("#bestFriend").html("<h3>Your New Best Food Friend: " + members[least].name + "</h3>");
-	// 		$("#bestFriend").append('<img src ="' + members[least].photo + '" alt="bestfriend-photo" class="best_pic">');
+		var challengeTitle = $("<h3>Day "+ posts[i].day + " - " + posts[i].date + "</h3>");
+			challengeTitle.addClass("challenge-title");
+			$("#challenge-div"+i).append(challengeTitle);
 
+		for (k=0; k<posts[i].projects.length; k++){
+			var challengeProjTitle = $("<h4> "+ posts[i].projects[k].title + "</h4>");
+			challengeProjTitle.addClass("challenge-proj-title");
+			$("#challenge-div"+i).append(challengeProjTitle);
+
+			for (m=0; m<posts[i].projects[k].info.length; m++){
+				var challengeProjPar = $("<p> "+ posts[i].projects[k].info[m] + "</p>");
+				challengeProjPar.addClass("challenge-proj-par");
+				$("#challenge-div"+i).append(challengeProjPar);
+			}
+		}
+	}
+	
 });
+
+
+
+// <div class="challenge-div">
+// 	<h3 class="challenge-title">Day 12 - April 12, 2017</h3>
+// 	<h4 class="challenge-proj-title">Bamazon, Node.js, MySQL</h4>
+// 	<p class="challenge-proj-par">Addition of MySQL table "departments"</p>
+// 	<p class="challenge-proj-par">Development and functionality to the Bamazon Sales Log</p>
+// </div>
+// <div class="challenge-div">
+// 	<h3 class="challenge-title">Day 13 - April 13, 2017</h3>
+// 	<h4 class="challenge-proj-title">Blog-Personal-Portfolio</h4>
+// 	<p class="challenge-proj-par">Updates to 100 Day Coding Challenge and start of Blog Post #2</p>
+// 	<h4 class="challenge-proj-title">Personal-Portfolio</h4>
+// 	<p class="challenge-proj-par">Changes to portfolio style sheet to add functionailty and compatable styles across all pages</p>
+// </div>
+
+
+
+
+
+
+
 
 
 
